3e71f764f7e3dc75626ab0693a0ecf544cb706dd
ChuckDevMtn <Chuck.DevMtn@gmail.com>
2017-03-23 20:16:43 -0600

    comitted from VS

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+var bodyParser = require('body-parser');
+var express = require('express');
 
+
+var app = express();
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+//This is acting as a temp storage, but is usually not involved in full scales apps // 
+var messages = [];
+//This is acting as a temp storage, but is usually not involved in full scales apps // 
+
+console.log(app)
+
+app.get('/messages', function(req, res, next) {
+    res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function(req, res, next) {
+    messages.push(req.body);
+    res.status(200).json({ messages: messages });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This always be the very last thing in your index.js file
+
+
+app.listen(3000, function() {
+    console.log('Listening on Port 3000')
+});
